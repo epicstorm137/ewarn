0c09b7d75189cc3c09e4e6515a37ab184f083111
Epicstorm <67658615+epicstorm137@users.noreply.github.com>
2022-01-08 11:51:18 +0000

    Add files via upload

---
+++ ewarn/lua/autorun/client/cl_warn.lua
@@ -0,0 +1,118 @@
+net.Receive("ewarn_open",function()
+    ewarn.OpenWarns()
+end)
+
+net.Receive("ewarn_plymsg",function()
+    chat.AddText(ewarn.red,ewarn.Config.ChatPrefix,ewarn.white,net.ReadString())
+end)
+
+local function getply(name)
+    for k,v in ipairs(player.GetAll()) do
+        if v:Nick() == name then
+            return v
+        end
+    end
+end
+
+function ewarn.OpenWarns()
+    local scrw,scrh = ScrW(),ScrH()
+
+    local frame = vgui.Create("DFrame")
+	frame:SetSize(0,0)
+	frame:Center()
+	frame:MakePopup()
+	frame:SetVisible(true)
+	frame:DockPadding(10,30,10,10)
+	ewarn.MakeFrame(frame,"EWarn Menu",scrw*.7,scrh*.6)
+
+    local bpannel = vgui.Create("DPanel",frame)
+    bpannel:Dock(BOTTOM)
+    bpannel:SetTall(scrw*.015)
+    bpannel:DockMargin(0,10,0,0)
+    bpannel.Paint = function() end
+
+    local lookup = vgui.Create("DTextEntry",bpannel)
+    lookup:Dock(LEFT)
+    lookup:SetWide(scrw*.075)
+    ewarn.MakeInput(lookup,"Search Player")
+
+    local stat = vgui.Create("DPanel",bpannel)
+    stat:Dock(FILL)
+    stat:DockMargin(5,0,0,0)
+    stat.Warn = "Select a Warning !"
+    stat.Paint = function(s,w,h)
+        surface.SetDrawColor(ewarn.bglight)
+		surface.DrawRect(0,0,w,h)
+        draw.SimpleText(stat.Warn,"WLFontUI",w/2,h/2,rainbow,TEXT_ALIGN_CENTER,TEXT_ALIGN_CENTER)
+    end
+
+    local players = vgui.Create("DListView",frame)
+    players:Dock(LEFT)
+    players:SetMultiSelect(false)
+    players:SetWide(scrw*.075)
+    players:AddColumn("Players",1)
+    players:SortByColumn(1,true)
+    ewarn.MakeList(players)
+    for k,v in ipairs(player.GetAll(),false) do
+        local pline = players:AddLine(v:Nick())
+    end
+
+    local warns = vgui.Create("DListView",frame)
+    warns:Dock(FILL)
+    warns:SetMultiSelect(false)
+    warns:DockMargin(5,0,0,0)
+    warns:AddColumn("Index",1)
+    warns:AddColumn("Date",2)
+    warns:AddColumn("Name",3)
+    warns:AddColumn("Reason",4)
+    warns:AddColumn("Admin Name",5)
+    warns:AddColumn("Admin SteamID",6)
+    ewarn.MakeList(warns)
+
+    players.OnRowSelected = function( panel, index, row )
+        local plyname = row:GetValue(1)
+        local target = getply(plyname)
+        if !ewarn.Config.AllowedRanks[LocalPlayer():GetUserGroup()] then
+            if target != LocalPlayer() then warns:Clear() return end
+        end
+        net.Start("ewarn_get")
+        net.WriteEntity(target)
+        net.SendToServer()
+        net.Receive("ewarn_send",function()
+            local data = util.JSONToTable(util.Decompress(net.ReadData(1048000)))
+            warns:Clear()
+            for k,v in ipairs(data) do
+                local info = util.JSONToTable(v.data)
+                local line = warns:AddLine(v.id, info.Date ,info.Name, info.Reason, info.AdminName, info.AdminID)
+                line.OnSelect = function()
+                    stat.Warn = "Reason: "..line:GetColumnText(4)
+                end
+                line.OnRightClick = function()
+                    local rightclick = DermaMenu()
+                    rightclick:AddOption("Copy Reason",function() SetClipboardText(line:GetColumnText(4)) end)
+                    rightclick:AddOption("Copy Admin's SteamID",function() SetClipboardText(line:GetColumnText(6)) end)
+
+                    if ewarn.Config.AllowedRanks[LocalPlayer():GetUserGroup()] then
+                        rightclick:AddOption("Remove Warning",function()
+                            net.Start("ewarn_delete")
+                            net.WriteInt(line:GetColumnText(1),32)
+                            net.WriteEntity(target)
+                            net.SendToServer()
+                        end)
+                        rightclick:AddOption("Edit Warning",function()
+                            Derma_StringRequest("EWarn - Edit","Enter the new reason:",info.Reason,function(text)
+                                net.Start("ewarn_edit")
+                                net.WriteInt(line:GetColumnText(1),32)
+                                net.WriteEntity(target)
+                                net.WriteString(text)
+                                net.SendToServer()
+                            end)
+                        end)
+                    end
+                    rightclick:Open(gui.MouseX(),gui.MouseY())
+                end
+            end
+        end)
+        warns:SortByColumn(1,true)
+    end
+end
---
+++ ewarn/lua/autorun/sh_warnloader.lua
@@ -0,0 +1,26 @@
+ewarn = ewarn or {}
+ewarn.Config = ewarn.Config or {}
+
+-- Prefix for chat messages, make sure to leave a space after
+ewarn.Config.ChatPrefix = "[EWarn] "
+
+-- If enabled, players will be able to see their own warnings
+ewarn.Config.PlayersCanSee = true
+
+-- If enabled, warns will be added when you get kicked
+ewarn.Config.LogKick = true
+
+-- If enabled, warns will be added when you get kicked
+ewarn.Config.LogBan = true
+
+-- Allowed ranks to open the warning menu and edit/remove warnings
+ewarn.Config.AllowedRanks = {
+    ["superadmin"] = true,
+    ["admin"] = true,
+}
+
+-- Commands that will open the menu
+ewarn.Config.Commands = {
+    ["/warns"] = true,
+    ["/warnings"] = true,
+}
